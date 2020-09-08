0eb2c1a47123f86dc622bf99f8074e8bda810b4c
Hayden Davis <1hayden.davis@gmail.com>
2020-09-08 01:10:36 -0400

    with graphql query

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,11 @@ const awsmobile = {
     "aws_cognito_region": "us-west-2",
     "aws_user_pools_id": "us-west-2_TGajVUJ2y",
     "aws_user_pools_web_client_id": "1cuda7qimu6dhv7roj8okhhmo",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://xsgoa2ojufewlgajn5koccfddq.appsync-api.us-west-2.amazonaws.com/graphql",
+    "aws_appsync_region": "us-west-2",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-t3jpe24bu5ffdmv3nvsb4asb2y"
 };
 
 
